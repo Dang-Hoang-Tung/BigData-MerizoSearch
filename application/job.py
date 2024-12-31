cb3c1b85caea1a8b43e64c3024732577f0fc6f09
Tony Dang <tungtonydang@gmail.com>
2024-12-31 21:36:05 +0000

    Add merizo codebase

--- application/job.py
+++ application/job.py
@@ -15,10 +15,10 @@ data_dir = "/test"
 # print(filenames)
 # print("Done")
 
-pipeline('/test/AF-Q46871-F1-model_v4.pdb', 'AF-Q46871-F1-model_v4.pdb', "/test-outputs-1")
+pipeline('/test/AF-Q46871-F1-model_v4.pdb', 'AF-Q46871-F1-model_v4.pdb', "/")
 print("Done 1")
 
-cmd = ['python', 'test/pipeline_script.py', '/test', '/test-outputs-2']
+cmd = ['python', 'test/pipeline_script.py', '/test', '/']
 print(f'STEP 0: RUNNING PIPELINE: {" ".join(cmd)}')
 p = Popen(cmd, stdin=PIPE,stdout=PIPE, stderr=PIPE)
 out, err = p.communicate()
