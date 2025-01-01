494c4b1af099d3198543df33e0c4f08f9b93e8a0
Tony Dang <tungtonydang@gmail.com>
2025-01-01 20:23:06 +0000

    Update code

--- application/job.py
+++ application/job.py
@@ -1,7 +1,7 @@
-from pipeline.pipeline_script import run_pipeline
+from pipeline.pipeline_script import pipeline
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
-
+import os
 from subprocess import Popen, PIPE
 
 spark = SparkSession.builder.appName("MerizoSearch").getOrCreate()
@@ -24,6 +24,31 @@ data_dir = "/test"
 # out, err = p.communicate()
 # print(out.decode("utf-8"))
 # print("Done 2")
+
+def read_dir(input_dir, output_dir):
+    """
+    Function reads a fasta formatted file of protein sequences
+    """
+    print("Getting file list")
+    rdd = sc.wholeTextFiles(os.path.join(input_dir, "*.pdb"))
+    # file_ids = list(glob.glob(input_dir+"*.pdb"))
+    file_paths = rdd.keys().collect()
+    analysis_files = []
+    for file in file_paths:
+        id = os.path.basename(file)
+        analysis_files.append([file, id, output_dir])
+    return(analysis_files)
+
+def run_pipeline(input_dir, output_dir):
+    # print(sys.argv[1], sys.argv[2])
+    pdbfiles = read_dir(input_dir, output_dir)
+    print(pdbfiles)
+    rdd = spark.sparkContext.parallelize(pdbfiles)
+    print(rdd)
+    print("STARTING")
+    results = rdd.map(lambda x: pipeline(x[0], x[1], x[2])).collect()
+    print(results)
+    print("DONE")
 
 run_pipeline(data_dir, "")
 
--- application/pipeline/pipeline_script.py
+++ application/pipeline/pipeline_script.py
@@ -3,16 +3,11 @@ from subprocess import Popen, PIPE
 import glob
 import os
 import multiprocessing
-from pyspark import SparkContext
-from pyspark.sql import SparkSession
 
 """
 usage: python pipeline_script.py [INPUT DIR] [OUTPUT DIR]
 approx 5seconds per analysis
 """
-
-spark = SparkSession.builder.appName("MerizoSearch").getOrCreate()
-sc = spark.sparkContext
 
 def run_parser(input_file, output_dir):
     """
@@ -50,20 +45,6 @@ def run_merizo_search(input_file, id):
     out, err = p.communicate()
     print(out.decode("utf-8"))
     print(err.decode("utf-8"))
-    
-def read_dir(input_dir, output_dir):
-    """
-    Function reads a fasta formatted file of protein sequences
-    """
-    print("Getting file list")
-    rdd = sc.wholeTextFiles(os.path.join(input_dir, "*.pdb"))
-    # file_ids = list(glob.glob(input_dir+"*.pdb"))
-    file_paths = rdd.keys().collect()
-    analysis_files = []
-    for file in file_paths:
-        id = os.path.basename(file)
-        analysis_files.append([file, id, output_dir])
-    return(analysis_files)
 
 def pipeline(filepath, id, outpath):
     # STEP 1
@@ -71,21 +52,10 @@ def pipeline(filepath, id, outpath):
     # STEP 2
     run_parser(id, outpath)
 
-def run_pipeline(input_dir, output_dir):
-    # print(sys.argv[1], sys.argv[2])
-    pdbfiles = read_dir(input_dir, output_dir)
-    print(pdbfiles)
-    rdd = spark.sparkContext.parallelize(pdbfiles)
-    print(rdd)
-    print("STARTING")
-    results = rdd.map(lambda x: pipeline(x[0], x[1], x[2])).collect()
-    print(results)
-    print("DONE")
-
-if __name__ == "__main__":
-    print(sys.argv[1], sys.argv[2])
-    pdbfiles = read_dir(sys.argv[1])
-    print(pdbfiles)
+# if __name__ == "__main__":
+#     print(sys.argv[1], sys.argv[2])
+#     pdbfiles = read_dir(sys.argv[1])
+#     print(pdbfiles)
     # rdd = spark.sparkContext.parallelize(pdbfiles)
     # rdd.map(lambda x: pipeline(x[0], x[1], x[2])).collect()
     # p = multiprocessing.Pool(1)
