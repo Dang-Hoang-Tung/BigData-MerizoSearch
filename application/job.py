2a17d2127dfbf7bef4396ff74b79af694287dbef
Tony Dang <tungtonydang@gmail.com>
2025-01-02 01:46:51 +0000

    Create merizo_adapter

--- application/job.py
+++ application/job.py
@@ -1,69 +1,41 @@
-from pipeline.pipeline_script import pipeline
+from pipeline.merizo_adapter import run_merizo
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
-import os
-from subprocess import Popen, PIPE
-from io import BytesIO
+# import os
+# from subprocess import Popen, PIPE
+# from io import BytesIO
 
 spark = SparkSession.builder.appName("MerizoSearch").getOrCreate()
 sc = spark.sparkContext
 
-data_dir = "/test"
+# data_dir = "/test"
 
-# filenames = run_test(sc, data_dir)
+# def read_dir(input_dir, output_dir):
+#     """
+#     Function reads a fasta formatted file of protein sequences
+#     """
+#     print("Getting file list")
+#     rdd = sc.wholeTextFiles(os.path.join(input_dir, "*.pdb"))
+#     # file_ids = list(glob.glob(input_dir+"*.pdb"))
+#     file_paths = rdd.keys().collect()
+#     analysis_files = []
+#     for file in file_paths:
+#         id = os.path.basename(file)
+#         analysis_files.append([file, id, output_dir])
+#     return(analysis_files)
 
-# print("Outside")
-# print(filenames)
-# print("Done")
+# def run_pipeline(input_dir, output_dir):
+#     pdb_files = sc.binaryFiles(data_dir).map(lambda x: pipeline(BytesIO(x[1]), os.path.basename(x[0]), "/home/almalinux/")).collect()
 
-# pipeline('/test/AF-Q46871-F1-model_v4.pdb', 'AF-Q46871-F1-model_v4.pdb', "~/output")
-# print("Done 1")
+#     print(pdb_files)
 
-# cmd = ['python', 'pipeline/pipeline_script.py', data_dir, 'file:///home/almalinux/output/']
-# print(f'STEP 0: RUNNING PIPELINE: {" ".join(cmd)}')
-# p = Popen(cmd, stdin=PIPE,stdout=PIPE, stderr=PIPE)
-# out, err = p.communicate()
-# print(out.decode("utf-8"))
-# print("Done 2")
-def read_dir(input_dir, output_dir):
-    """
-    Function reads a fasta formatted file of protein sequences
-    """
-    print("Getting file list")
-    rdd = sc.wholeTextFiles(os.path.join(input_dir, "*.pdb"))
-    # file_ids = list(glob.glob(input_dir+"*.pdb"))
-    file_paths = rdd.keys().collect()
-    analysis_files = []
-    for file in file_paths:
-        id = os.path.basename(file)
-        analysis_files.append([file, id, output_dir])
-    return(analysis_files)
+# run_pipeline(data_dir, "/home/almalinux/")
 
-def run_pipeline(input_dir, output_dir):
-    # print(sys.argv[1], sys.argv[2])
-    # pdbfiles = read_dir(input_dir, output_dir)
-    # print(pdbfiles)
-    pdb_files = sc.binaryFiles(data_dir).map(lambda x: pipeline(BytesIO(x[1]), os.path.basename(x[0]), "/home/almalinux/")).collect()
+# Directory containing text files
+input_directory = "/test"  # or a local path like "/path/to/directory"
 
-    print(pdb_files)
-    # pdb_files_bytes.map(lambda x: pipeline(x[0], x[1], x[2])).collect()
-    # rdd = spark.sparkContext.parallelize(pdbfiles)
-    # print(rdd)
-    # print("STARTING")
-    # results = rdd.map(lambda x: pipeline(x[0], x[1], x[2])).collect()
-    # print(results)
-    # print("DONE")
+# Read all text files in the directory
+files_rdd = sc.wholeTextFiles(input_directory)
+result = files_rdd.map(lambda x: run_merizo(x[0], x[1])).collect()
 
-run_pipeline(data_dir, "/home/almalinux/")
-
-# results_human = analyse_spark("/analysis_data/UP000005640_9606_HUMAN_v4", sc, "arms")
-# results_ecoli = analyse_spark("/analysis_data/UP000000625_83333_ECOLI_v4.tar", sc, "arms")
-
-# data = []
-# columns = ["year", "count"]
-# for a in results.keys():
-#     data.append([a, results[a]])
-# df = spark.createDataFrame(data, columns).coalesce(1)
-# df.show()
-# df.write.option("header","true").mode("overwrite").csv("/1850_1859_sparksubmit")
-
+print(result)
---
+++ application/pipeline/merizo_adapter.py
@@ -0,0 +1,18 @@
+import os
+
+ADAPTER_DIR = "/home/almalinux/merizo_files"
+
+def write_file_to_adapter_dir(file_name, file_content):
+    # Ensure the directory exists
+    os.makedirs(ADAPTER_DIR, exist_ok=True)
+    # Construct the full file path
+    file_path = os.path.join(ADAPTER_DIR, file_name)
+    
+    # Write the file contents
+    with open(file_path, 'w') as f:
+        f.write(file_content)
+    
+    return file_path
+
+def run_merizo(file_name, file_content):
+    write_file_to_adapter_dir(file_name, file_content)
