94522a21dbc1c28061e3638781039f591c75bdca
Tony Dang <tungtonydang@gmail.com>
2025-01-03 05:38:06 +0000

    Fix bugs and keep it simple stupid

--- application/spark_job.py
+++ application/spark_job.py
@@ -22,26 +22,23 @@ HUMAN_SUMMARY_OUTPUT_PATH = "/human_cath_summary.csv"
 
 PLDDT_MEANS_OUTPUT_PATH = "/plDDT_means.csv"
 
-FILE_ID_KEY = "__FILE_ID__"
 MEAN_PLDDT_KEY = "__MEAN_PLDDT__"
-MEANS_DATA_KEY = "__MEANS_DATA__"
 
 def process_file(file_entry, dataset: str):
     file_id = os.path.basename(file_entry[0])
     file_content = file_entry[1]
-    return merizo_adapter(file_id, file_content, dataset, FILE_ID_KEY, MEAN_PLDDT_KEY)
+    return merizo_adapter(file_id, file_content, dataset, MEAN_PLDDT_KEY)
 
 def combine_dict(acc_dict: dict, new_dict: dict):
-    # Accumulate the mean plddt values
-    if MEANS_DATA_KEY not in acc_dict:
-        acc_dict[MEANS_DATA_KEY] = []
-        acc_dict[MEANS_DATA_KEY].append([acc_dict[FILE_ID_KEY], acc_dict[MEAN_PLDDT_KEY]])
-
-    acc_dict[MEANS_DATA_KEY].append([new_dict[FILE_ID_KEY], new_dict[MEAN_PLDDT_KEY]])
-
-    # Accumulate the counts for each cath_id
     for key in new_dict:
-        if key != FILE_ID_KEY and key != MEAN_PLDDT_KEY:
+        # Accumulate the mean plddt values
+        if key == MEAN_PLDDT_KEY:
+            if key in acc_dict:
+                acc_dict[key].extend(new_dict[key])
+            else:
+                acc_dict[key] = new_dict[key]
+        # Accumulate the counts for each cath_id
+        else:
             if key in acc_dict:
                 acc_dict[key] += new_dict[key]
             else:
@@ -58,14 +55,14 @@ def write_summary_to_file(results: dict, file_path: str):
     data = []
     column_headers = ["cath_code", "count"]
     for a in results.keys():
-        if a != MEANS_DATA_KEY:
+        if a != MEAN_PLDDT_KEY:
             data.append([a, results[a]])
     df = spark.createDataFrame(data, column_headers).coalesce(1).sort("cath_code")
     df.write.option("header","true").mode("overwrite").csv(file_path)
     return df
 
 def get_plddt_means(means_data: list):
-    column_headers = [FILE_ID_KEY, MEAN_PLDDT_KEY]
+    column_headers = [MEAN_PLDDT_KEY]
     df = spark.createDataFrame(means_data, column_headers)
     [mean, stddev] = df.select(
         F.mean(MEAN_PLDDT_KEY).alias('mean'),
@@ -90,7 +87,7 @@ def write_mean_plddt_to_file(ecoli_means_data: list, human_means_data: list, fil
 test_results = distribute_tasks(TEST_DATASET, TEST_DATASET_HDFS_DIR)
 test_summary_df = write_summary_to_file(test_results, TEST_SUMMARY_OUTPUT_PATH)
 test_summary_df.show()
-test_means_df = write_mean_plddt_to_file(test_results[MEANS_DATA_KEY], test_results[MEANS_DATA_KEY], TEST_MEANS_OUTPUT_PATH)
+test_means_df = write_mean_plddt_to_file(test_results[MEAN_PLDDT_KEY], test_results[MEAN_PLDDT_KEY], TEST_MEANS_OUTPUT_PATH)
 test_means_df.show()
 
 # # Process the ECOLI dataset
@@ -103,7 +100,7 @@ test_means_df.show()
 # human_summary_df = write_summary_to_file(human_results, HUMAN_SUMMARY_OUTPUT_PATH)
 # human_summary_df.show()
 
-# combined_means_df = write_mean_plddt_to_file(ecoli_results[MEANS_DATA_KEY], human_results[MEANS_DATA_KEY], PLDDT_MEANS_OUTPUT_PATH)
+# combined_means_df = write_mean_plddt_to_file(ecoli_results[MEAN_PLDDT_KEY], human_results[MEAN_PLDDT_KEY], PLDDT_MEANS_OUTPUT_PATH)
 # combined_means_df.show()
 
 print("ALL DONE")
