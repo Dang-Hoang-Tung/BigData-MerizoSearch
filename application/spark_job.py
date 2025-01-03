276250c833073bc8f267a513b8f83673ed5b201d
Tony Dang <tungtonydang@gmail.com>
2025-01-03 03:51:18 +0000

    Refactor for safety

--- application/spark_job.py
+++ application/spark_job.py
@@ -20,15 +20,18 @@ HUMAN_DATASET = "UP000005640_9606_HUMAN_v4"
 HUMAN_DATASET_HDFS_DIR = f"/{HUMAN_DATASET}"
 HUMAN_SUMMARY_OUTPUT_PATH = "/human_cath_summary.csv"
 
+MEAN_PLDDT_KEY = "__MEAN_PLDDT__"
+PLDDT_MEANS_OUTPUT_PATH = "/plDDT_means.csv"
+
 def process_file(file_entry, dataset: str):
     file_id = os.path.basename(file_entry[0])
     file_content = file_entry[1]
-    return merizo_adapter(file_id, file_content, dataset)
+    return merizo_adapter(file_id, file_content, dataset, MEAN_PLDDT_KEY)
 
 def combine_dict(acc_dict: dict, new_dict: dict):
     for key in new_dict:
         # Accumulate the mean plddt values
-        if key == "__MEAN_PLDDT__":
+        if key == MEAN_PLDDT_KEY:
             if key in acc_dict:
                 acc_dict[key].append(new_dict[key])
             else:
@@ -51,7 +54,7 @@ def write_summary_to_file(results: dict, file_path: str):
     data = []
     columns = ["cath_code", "count"]
     for a in results.keys():
-        if a != "__MEAN_PLDDT__":
+        if a != MEAN_PLDDT_KEY:
             data.append([a, results[a]])
     df = spark.createDataFrame(data, columns).coalesce(1).sort("cath_code")
     df.write.option("header","true").mode("overwrite").csv(file_path)
@@ -67,7 +70,7 @@ def write_mean_plddt_to_file(ecoli_means: list, human_means: list, file_path: st
 test_results = distribute_tasks(TEST_DATASET, TEST_DATASET_HDFS_DIR)
 test_summary_df = write_summary_to_file(test_results, TEST_SUMMARY_OUTPUT_PATH)
 test_summary_df.show()
-test_means_df = write_mean_plddt_to_file(test_results["__MEAN_PLDDT__"], test_results["__MEAN_PLDDT__"], "test_mean_plddt.csv")
+test_means_df = write_mean_plddt_to_file(test_results[MEAN_PLDDT_KEY], test_results[MEAN_PLDDT_KEY], PLDDT_MEANS_OUTPUT_PATH)
 test_means_df.show()
 
 # # Process the ECOLI dataset
