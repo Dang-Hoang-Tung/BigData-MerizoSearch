8c9a2a4437876b849399ea670a5eec8a53b064bd
Tony Dang <tungtonydang@gmail.com>
2025-01-05 07:57:47 +0000

    Fix type errors with AnalysisResults

--- application/pipeline/globals.py
+++ application/pipeline/globals.py
@@ -3,7 +3,6 @@ Global variables and types for the Spark application
 """
 
 from dataclasses import dataclass
-from collections.abc import MutableMapping
 
 @dataclass
 class JobInputs:
@@ -17,8 +16,9 @@ class JobInputs:
     means_output_path: str
 
 @dataclass
-class AnalysisResults(MutableMapping):
+class AnalysisResults(dict):
     """
+    Dict-like class for type safety (bunch of methods that just implement the dict functionality).
     Represents the results of a distributed task (invoking the Merizo Search pipeline).
     Dynamically add {cath_code: count} results.
     """
@@ -30,26 +30,56 @@ class AnalysisResults(MutableMapping):
         if self.mean_plddt_list is None:
             self.mean_plddt_list = []
 
-    def __init__(self):
-        self._data = {}
+    def __setitem__(self, key, item):
+        self.__dict__[key] = item
 
     def __getitem__(self, key):
-        return self._data[key]
-
-    def __setitem__(self, key, value):
-        self._data[key] = value
-
-    def __delitem__(self, key):
-        del self._data[key]
-
-    def __iter__(self):
-        return iter(self._data)
-
-    def __len__(self):
-        return len(self._data)
+        return self.__dict__[key]
 
     def __repr__(self):
-        return f"{self._data}"
+        return repr(self.__dict__)
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __delitem__(self, key):
+        del self.__dict__[key]
+
+    def clear(self):
+        return self.__dict__.clear()
+
+    def copy(self):
+        return self.__dict__.copy()
+
+    def has_key(self, k):
+        return k in self.__dict__
+
+    def update(self, *args, **kwargs):
+        return self.__dict__.update(*args, **kwargs)
+
+    def keys(self):
+        return self.__dict__.keys()
+
+    def values(self):
+        return self.__dict__.values()
+
+    def items(self):
+        return self.__dict__.items()
+
+    def pop(self, *args):
+        return self.__dict__.pop(*args)
+
+    def __cmp__(self, dict_):
+        return self.__cmp__(self.__dict__, dict_)
+
+    def __contains__(self, item):
+        return item in self.__dict__
+
+    def __iter__(self):
+        return iter(self.__dict__)
+
+    def __unicode__(self):
+        return unicode(repr(self.__dict__))
 
 @dataclass
 class PlddtMeans:
